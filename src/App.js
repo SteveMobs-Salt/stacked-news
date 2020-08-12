c876f931e0c2240b1270735d197aefaf7b666f5c
Brenda Cecato <brendycm@gmail.com>
2020-08-12 14:41:13 +0200

    Added Bulma and tried layout spaces

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import Navbar from './Components/Navbar'
+import CategoryList from './Components/CategoryList'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container is-narrow"><div className="columns"><CategoryList /></div></div>
+      {/* <div>My text is long and beautiful, as, it has been well documented, are various other parts of my website. 
+        My placeholder text, I think, is going to end up being very good with women. 
+        Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites.
+
+      I’m the best thing that ever happened to placeholder text.
+
+      Some people have an ability to write placeholder text... 
+      It's an art you're basically born with. You either have it or you don't. 
+      We are going to make placeholder text great again. Greater than ever before.
+
+      I know words. I have the best words. An ‘extremely credible source’ 
+      has called my office and told me that Barack Obama’s placeholder text is a fraud. 
+      Lorem Ispum is a choke artist. It chokes!
+
+The best taco bowls are made in Trump Tower Grill. I love Hispanics! 
+We have so many things that we have to do better... and certainly ipsum is one of them. 
+I think the only card she has is the Lorem card. An ‘extremely credible source’ 
+has called my office and told me that Barack Obama’s placeholder text is a fraud. </div> */}
     </div>
   );
 }
---
+++ src/Components/CategoryList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function CategoryList() {
+
+  return (
+    <div className="column is-one-quarter">
+    <aside class="menu">
+      <p class="menu-label">
+        General
+  </p>
+      <ul class="menu-list">
+        <li><a>Dashboard</a></li>
+        <li><a>Customers</a></li>
+      </ul>
+    </aside>
+    </div>
+  )
+}
+
+export default CategoryList;
