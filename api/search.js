d33923fbcee2b4a112420373ed267065029bfa84
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-08-13 16:11:05 +0200

    Searchbar now able to search for news. Single word only.

--- api/search.js
+++ api/search.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch';
 
 module.exports = (req, res) => {
-  const {category} = req.query;
-  fetch(`https://content.guardianapis.com/${category}?api-key=${process.env.REACT_APP_API_KEY}&show-fields=all`).then(resp => resp.json())
+  const {query} = req.query;
+  fetch(`https://content.guardianapis.com/search?q=${query}&api-key=${process.env.REACT_APP_API_KEY}&show-fields=all`).then(resp => resp.json())
   .then( data => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
--- src/App.js
+++ src/App.js
@@ -8,44 +8,65 @@ import newsResults from './mockDB/allFieldsResult';
 require('dotenv').config();
 
 function App() {
-  const[news, setNews] = useState([]);
+  const [news, setNews] = useState([]);
   const [category, setCategory] = useState('international');
-  const [search, setSearch] = useState('');
-  
-  useEffect(() => {
-    setNews(newsResults[0].response.results);
-  },[]);
+  const [search, setSearch] = useState(null);
 
   useEffect(() => {
+    setNews(newsResults[0].response.results);
+  }, []);
+
+  useEffect(() => {
+    const fetchCategoryNews = () => {
+      fetch(`/api/category?category=${category}`)
+        .then(res => {
+          console.log(res);
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+          setNews(data.response.results);
+        })
+        .catch(err => console.log('error from react= ', err));
+    };
+    const fetchSearchNews = (query) => {
+      // if(search.includes(' ')) {
+      //   setSearch(search.replace(' ', '%20'))
+      // }
+      fetch(`/api/search?query=${search}`)
+        .then(res => {
+          console.log(res);
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+          setNews(data.response.results);
+        })
+        .catch(err => console.log('error from react= ', err));
+    };
+    if (search) {
+      fetchSearchNews();
+    }
     if (category !== 'international') {
       fetchCategoryNews();
     }
-  }, [category])
-  
-  const fetchCategoryNews = () => {
-    fetch(`/api/category?category=${category}`)
-      .then(res => res.json())
-      .then(data => setNews(data.response.results))
-      .catch(err => console.log(err))
-  }
+  }, [category, search]);
 
   return (
     <div>
-      <Navbar setSearch={setSearch}/>
+      <Navbar setSearch={setSearch} />
       <div className="columns is-centered mt-4">
         <div className="column is-three-fifths">
           <div className="container is-fluid">
             <div className="columns is-centered">
-              <CategoryList setCategory={setCategory}/>
+              <CategoryList setCategory={setCategory} />
               <div className="column is-half">
-              <div className="box">
-                <NewsList newsListArray={news}/>
+                <div className="box">
+                  <NewsList newsListArray={news} />
                 </div>
               </div>
               <div className="column is-one-quarter">
-                <div className="box">
-                  asdasdasdas
-                </div>
+                <div className="box">asdasdasdas</div>
               </div>
             </div>
           </div>
