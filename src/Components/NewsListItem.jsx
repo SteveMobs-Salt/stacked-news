e800ee5ed500ec471567c3dfc53d1217a9c106cd
Inge <46241840+Inge-bot@users.noreply.github.com>
2020-08-14 09:46:02 +0200

    Changed location of book icon and removed share icon.

--- src/Components/NewsListItem.jsx
+++ src/Components/NewsListItem.jsx
@@ -16,6 +16,15 @@ const NewsListItem = ({ title, subline, date, img, url, id, read }) => {
               <a href={url}>
                 <strong>{title}</strong>
               </a>
+              {/* <nav class="level is-mobile">
+            <div class="level-left"> */}
+              <a class="book-icon ml-3" aria-label="like" onClick={() => read(id)}>
+                <span class="icon is-small is-size-6">
+                  <i class="fa fa-book fa-lg" aria-hidden="true"></i>
+                </span>
+              </a>
+            {/* </div>
+          </nav> */}
               <br />
               <small className="my-6 py-6 has-text-weight-medium">
                 {moment(date).format('dddd, MMMM Do YYYY')}
@@ -24,20 +33,6 @@ const NewsListItem = ({ title, subline, date, img, url, id, read }) => {
             </p>
             <p>{subline.replace(/<strong>|<br>|<\/strong>/g, '')}</p>
           </div>
-          <nav class="level is-mobile">
-            <div class="level-left">
-              <a class="level-item" aria-label="reply">
-                <span class="icon is-small">
-                  <i class="fa fa-share" aria-hidden="true"></i>
-                </span>
-              </a>
-              <a class="level-item" aria-label="like" onClick={() => read(id)}>
-                <span class="icon is-small">
-                  <i class="fa fa-book" aria-hidden="true"></i>
-                </span>
-              </a>
-            </div>
-          </nav>
         </div>
       </article>
     </div>
