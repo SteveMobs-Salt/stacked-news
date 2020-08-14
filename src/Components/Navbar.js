b37e7b888b34f3e6af7a1471edc9b78407053691
Brenda Cecato <brendycm@gmail.com>
2020-08-14 10:44:22 +0200

    Fixed linting errors and position of weather-mock

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,25 +9,10 @@ function Navbar({ setSearch }) {
       aria-label="main navigation"
     >
       <div className="navbar-brand ml-5 pl-5">
-        <a
-          className="navbar-item has-text-white has-text-weight-bold title is-2 is-family-monospace"
-          href="#"
-        >
+        <a className="navbar-item has-text-white has-text-weight-bold title is-2 is-family-monospace" href="# ">
           STACKED NEWS
         </a>
-        {/* <a
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a> */}
       </div>
-
       <div id="navbarBasicExample" className="navbar-menu">
         {/* <div className="navbar-start">
           <a className="navbar-item has-text-white">Home</a>
